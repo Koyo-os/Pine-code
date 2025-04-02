b044472dde31c9560d97f8c3219c276098919dc7
osamikoyo <v.pylayev@mail.ru>
2025-04-02 14:45:18 +0500

    update

--- ininter.go
+++ ininter.go
@@ -1,10 +1,37 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
-func initPiner() error {
+const templ = `
+type: "piner"
+addr: "%s"
+
+exclude_files :
+  - ""
+exclude_dirs :
+  - "test"
+  - "cmd"
+  - "build"
+include_dirs :
+  - "all"
+temp_dir : "temp"
+	`
+
+func initPiner(addr string) error {
 	file, err := os.Create("pine.yaml")
 	if err != nil {
 		return err
 	}
+
+	text := fmt.Sprintf(templ, addr)
+	_, err = io.WriteString(file, text)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
